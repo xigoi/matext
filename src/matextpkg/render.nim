78f5b8d8fb94b48e8bd1e25b343dbd865069537d
Adam Blažek <xigoi@disroot.org>
2022-02-02 19:11:04 +0100

    Binary operators

--- src/matextpkg/render.nim
+++ src/matextpkg/render.nim
@@ -4,14 +4,40 @@ import std/sequtils
 import std/strutils
 import std/sugar
 
+const binaryOperators = {
+  "cdot": "⋅", "gtrdot": "⋗",
+  "cdotp": "⋅", "intercal": "⊺",
+  "centerdot": "⋅", "land": "∧", "rhd": "⊳",
+  "circ": "∘", "leftthreetimes": "⋋", "rightthreetimes": "⋌",
+  "amalg": "⨿", "circledast": "⊛", "ldotp": ".", "rtimes": "⋊",
+  "And": "&", "circledcirc": "⊚", "lor": "∨", "setminus": "∖",
+  "ast": "∗", "circleddash": "⊝", "lessdot": "⋖", "smallsetminus": "∖",
+  "barwedge": "⊼", "Cup": "⋓", "lhd": "⊲", "sqcap": "⊓",
+  "bigcirc": "◯", "cup": "∪", "ltimes": "⋉", "sqcup": "⊔",
+  "mod": "bmod", "curlyvee": "⋎", "times": "×",
+  "boxdot": "⊡", "curlywedge": "⋏", "mp": "∓", "unlhd": "⊴",
+  "boxminus": "⊟", "div": "÷", "odot": "⊙", "unrhd": "⊵",
+  "boxplus": "⊞", "divideontimes": "⋇", "ominus": "⊖", "uplus": "⊎",
+  "boxtimes": "⊠", "dotplus": "∔", "oplus": "⊕", "vee": "∨",
+  "bullet": "∙", "doublebarwedge": "⩞", "otimes": "⊗", "veebar": "⊻",
+  "Cap": "⋒", "doublecap": "⋒", "oslash": "⊘", "wedge": "∧",
+  "cap": "∩", "doublecup": "⋓", "pm": "±", "plusmn": "±", "wr": "≀",
+}.mapIt((cmd: "\\" & it[0], op: it[1]))
+
 var atom = fwdcl[TextRect]()
 let expr = atom.atLeast(1).map(atoms => atoms.foldl(a & b))
 let oneChar = alphanumeric.map(ch => ($ch).toTextRect)
+let binaryOp = oneOf(
+  c"+-*/".map(ch => $ch),
+  !nop[string](),
+  binaryOperators.map(it => s(it.cmd).result(it.op))
+).map(s => (" " & s & " ").toTextRect)
 let frac = s"\frac" >> (atom & atom).map((fraction: seq[TextRect]) => (
   let numerator = fraction[0]
   let denominator = fraction[1]
   let fractionLine = "─".repeat(max(numerator.width, denominator.width)).toTextRect
   stack(numerator, fractionLine, denominator, numerator.height, saCenter)
 ))
-atom.become oneChar | frac
-echo expr.parse("\\frac12").value
+let bracedExpr = c('{') >> expr << c('}')
+atom.become (bracedExpr | oneChar | binaryOp | frac) << whitespace.many
+echo expr.parse("\\frac{11 \\cdot 2}{3 + 4}").value
