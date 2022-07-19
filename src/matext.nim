8452d57ee149b591886ce45c18209ca99c5046fa
Adam Blažek <xigoi@disroot.org>
2022-07-20 00:37:52 +0200

    Better CLI

--- src/matext.nim
+++ src/matext.nim
@@ -14,7 +14,9 @@ elif isMainModule:
         stdout.writeLine stdin.readAll.render(oneLine = oneLine)
     except ValueError:
       stderr.writeLine "matext: error: " & getCurrentExceptionMsg()
-  dispatch matext
+  dispatch matext, short = {"oneLine": '1'}, help = {
+    "oneLine": "force the output to be on one line by mangling vertical notation"
+  }
 else:
   proc matext*(latex: string): string =
     latex.render
